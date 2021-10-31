204add96baef28f1a4c8f44ad941ec6d8cf6b2d0
shreyas0906 <shreyas0906@gmail.com>
2021-11-01 02:13:37 +0530

    removed duplicate scripts. cleaned up mediapipe_hands_demo_video.py

--- mediapipe_hands_demo_video.py
+++ mediapipe_hands_demo_video.py
@@ -17,89 +17,83 @@ print(f"FPS: {fps}")
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-
 with mp_hands.Hands(
-    min_detection_confidence=0.7,
-    min_tracking_confidence=0.7) as hands:
-  while cap.isOpened():
-    num_frames = 120
-    start = time.time()
-    success, image = cap.read()
+        min_detection_confidence=0.7,
+        min_tracking_confidence=0.7) as hands:
+    while cap.isOpened():
+        start = time.time()
+        success, image = cap.read()
 
-    image_width, image_height, _ = image.shape
-    print(f"image width: {image_width} and image height: {image_height}")
+        image_width, image_height, _ = image.shape
 
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
 
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
+        # Flip the image horizontally for a later selfie-view display, and convert
+        # the BGR image to RGB.
 
-    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    filler_image = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
-    # filler_image = np.zeros((480, 640, 3), dtype='uint8')
-    # filler_image[:,:,0] = gray_image
-    # filler_image[:,:,1] = gray_image
-    # filler_image[:,:,2] = gray_image
-    print(f"filler image: {filler_image.shape}")
-    
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    results = hands.process(filler_image) # filler_image
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        filler_image = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        results = hands.process(filler_image)  # filler_image
 
-    if results.multi_hand_landmarks:
-      # print(f" result: {results.multi_hand_landmarks}")
-      for hand_landmarks in results.multi_hand_landmarks:
-        for point in mp_hands.HandLandmark:
-          # print(point)
-          index_finger_mcp = np.array((int(hand_landmarks.landmark[point.INDEX_FINGER_MCP].x * image_height),
-           int(hand_landmarks.landmark[point.INDEX_FINGER_MCP].y * image_width)))
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-          pinky_finger_mcp = np.array((int(hand_landmarks.landmark[point.PINKY_MCP].x * image_height),
-          int(hand_landmarks.landmark[point.PINKY_MCP].y * image_width)))
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for point in mp_hands.HandLandmark:
+                    index_finger_mcp = np.array((int(hand_landmarks.landmark[point.INDEX_FINGER_MCP].x * image_height),
+                                                 int(hand_landmarks.landmark[point.INDEX_FINGER_MCP].y * image_width)))
 
-          middle_finger_mcp = np.array((int(hand_landmarks.landmark[point.MIDDLE_FINGER_MCP].x * image_height),
-          int(hand_landmarks.landmark[point.MIDDLE_FINGER_MCP].y * image_width)))
+                    pinky_finger_mcp = np.array((int(hand_landmarks.landmark[point.PINKY_MCP].x * image_height),
+                                                 int(hand_landmarks.landmark[point.PINKY_MCP].y * image_width)))
 
-          ring_finger_mcp = np.array((int(hand_landmarks.landmark[point.RING_FINGER_MCP].x * image_height),
-          int(hand_landmarks.landmark[point.RING_FINGER_MCP].y * image_width)))
+                    middle_finger_mcp = np.array(
+                        (int(hand_landmarks.landmark[point.MIDDLE_FINGER_MCP].x * image_height),
+                         int(hand_landmarks.landmark[point.MIDDLE_FINGER_MCP].y * image_width)))
 
-          thumb_cmc = np.array((int(hand_landmarks.landmark[point.THUMB_CMC].x * image_height),
-          int(hand_landmarks.landmark[point.THUMB_CMC].y * image_width)))
+                    ring_finger_mcp = np.array((int(hand_landmarks.landmark[point.RING_FINGER_MCP].x * image_height),
+                                                int(hand_landmarks.landmark[point.RING_FINGER_MCP].y * image_width)))
 
-          wrist = np.array((int(hand_landmarks.landmark[point.WRIST].x * image_height),
-          int(hand_landmarks.landmark[point.WRIST].y * image_width)))
+                    thumb_cmc = np.array((int(hand_landmarks.landmark[point.THUMB_CMC].x * image_height),
+                                          int(hand_landmarks.landmark[point.THUMB_CMC].y * image_width)))
 
-          normalizedLandmark = hand_landmarks.landmark[point]
-          pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x, normalizedLandmark.y, image_width, image_height)
+                    wrist = np.array((int(hand_landmarks.landmark[point.WRIST].x * image_height),
+                                      int(hand_landmarks.landmark[point.WRIST].y * image_width)))
 
-          mp_drawing.draw_landmarks(
-              image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    normalizedLandmark = hand_landmarks.landmark[point]
+                    pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x,
+                                                                                           normalizedLandmark.y,
+                                                                                           image_width, image_height)
 
-          vertices = np.array([[index_finger_mcp[0], index_finger_mcp[1]],
-                                [middle_finger_mcp[0], middle_finger_mcp[1]],
-                                [ring_finger_mcp[0], ring_finger_mcp[1]],
-                                [pinky_finger_mcp[0], pinky_finger_mcp[1]],
-                                [wrist[0], wrist[1]],
-                                [thumb_cmc[0], thumb_cmc[1]]], np.int32)
-          pts = vertices.reshape((-1, 1, 2))
-          cv2.polylines(image, [pts], isClosed=True, color=(0, 255, 255), thickness=1)
+                    mp_drawing.draw_landmarks(
+                        image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-          # fill it
-          cv2.fillPoly(image, [pts], color=(0, 255, 255))
+                    vertices = np.array([[middle_finger_mcp[0], middle_finger_mcp[1]],
+                                         [index_finger_mcp[0], index_finger_mcp[1]],
+                                         [ring_finger_mcp[0], ring_finger_mcp[1]],
+                                         [pinky_finger_mcp[0], pinky_finger_mcp[1]],
+                                         [wrist[0], wrist[1]],
+                                         [thumb_cmc[0], thumb_cmc[1]]], np.int32)
+                    pts = vertices.reshape((-1, 1, 2))
+                    cv2.polylines(image, [pts], isClosed=True, color=(0, 255, 255), thickness=1)
 
-          
+                    # fill it
+                    cv2.fillPoly(image, [pts], color=(0, 255, 255))
 
-    print("FPS: " , 1/(time.time() - start))
-    cv2.imshow('MediaPipe Hands', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+        cv2.imshow('MediaPipe Hands', image)
+
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            cap.release()
+
+
 cap.release()
