5dadff7d7aa91b07ad234c9368aed20e92f098fd
shreyas0906 <shreyas0906@gmail.com>
2021-11-01 03:40:44 +0530

    pose classification and hand points annotations are shown in different windows now.

--- src/live_test.py
+++ src/live_test.py
@@ -9,6 +9,8 @@ from tensorflow.keras.models import load_model
 
 
 class WebcamTest:
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
 
     def __init__(self, model):
         self.hands = mp.solutions.hands
@@ -60,6 +62,8 @@ class WebcamTest:
             while self.video.isOpened():
 
                 success, frames = self.video.read()
+                frame = frames.copy()
+                image_width, image_height, _ = frames.shape
 
                 if not success:
                     print("No camera connected.")
@@ -74,6 +78,52 @@ class WebcamTest:
                 image = cv2.cvtColor(frames, cv2.COLOR_RGB2BGR)
 
                 if results.multi_hand_landmarks:
+                    for hand_landmarks in results.multi_hand_landmarks:
+                        for point in WebcamTest.mp_hands.HandLandmark:
+                            index_finger_mcp = np.array(
+                                (int(hand_landmarks.landmark[point.INDEX_FINGER_MCP].x * image_height),
+                                 int(hand_landmarks.landmark[point.INDEX_FINGER_MCP].y * image_width)))
+
+                            middle_finger_mcp = np.array(
+                                (int(hand_landmarks.landmark[point.MIDDLE_FINGER_MCP].x * image_height),
+                                 int(hand_landmarks.landmark[point.MIDDLE_FINGER_MCP].y * image_width)))
+
+                            ring_finger_mcp = np.array(
+                                (int(hand_landmarks.landmark[point.RING_FINGER_MCP].x * image_height),
+                                 int(hand_landmarks.landmark[point.RING_FINGER_MCP].y * image_width)))
+
+                            pinky_finger_mcp = np.array((int(hand_landmarks.landmark[point.PINKY_MCP].x * image_height),
+                                                         int(hand_landmarks.landmark[point.PINKY_MCP].y * image_width)))
+
+                            thumb_cmc = np.array((int(hand_landmarks.landmark[point.THUMB_CMC].x * image_height),
+                                                  int(hand_landmarks.landmark[point.THUMB_CMC].y * image_width)))
+
+                            wrist = np.array((int(hand_landmarks.landmark[point.WRIST].x * image_height),
+                                              int(hand_landmarks.landmark[point.WRIST].y * image_width)))
+
+                            WebcamTest.mp_drawing.draw_landmarks(
+                                image, hand_landmarks, WebcamTest.mp_hands.HAND_CONNECTIONS)
+
+                            vertices = np.array([[index_finger_mcp[0], index_finger_mcp[1]],
+                                                 [middle_finger_mcp[0], middle_finger_mcp[1]],
+                                                 [ring_finger_mcp[0], ring_finger_mcp[1]],
+                                                 [pinky_finger_mcp[0], pinky_finger_mcp[1]],
+                                                 [wrist[0], wrist[1]],
+                                                 [thumb_cmc[0], thumb_cmc[1]]], dtype=np.int32)
+
+                            pts = vertices.reshape((-1, 1, 2))
+                            cv2.polylines(image, [pts], isClosed=True, color=(0, 255, 255), thickness=1)
+
+                            # fill the palm zone.
+                            cv2.fillPoly(image, [pts], color=(0, 255, 255))
+
+                            # horizontal line from index finger to pinky finger
+                            cv2.line(image, (index_finger_mcp[0], index_finger_mcp[1]),
+                                     (pinky_finger_mcp[0], pinky_finger_mcp[1]), (255, 0, 0), thickness=2)
+
+                            # vertical line from index finger to thumb
+                            cv2.line(image, (index_finger_mcp[0], index_finger_mcp[1]),
+                                     (thumb_cmc[0], thumb_cmc[1]), (0, 0, 255), thickness=2)
 
                     tmp1 = str(results.multi_hand_landmarks[0]).split()
                     coordinates_list = [elem for elem in tmp1 if elem != 'landmark' if elem != '{'
@@ -87,11 +137,12 @@ class WebcamTest:
                 else:
                     predicted_label = "No hand"
 
-                cv2.putText(image, predicted_label, (35, 35), self.font, 1, (0, 255, 255), 1, cv2.LINE_4)
-                cv2.putText(image, str("{0:.3f}".format((time.time() - start_time) * 100)) + 'ms',
+                cv2.putText(frame, predicted_label, (35, 35), self.font, 1, (0, 255, 255), 1, cv2.LINE_4)
+                cv2.putText(frame, str("{0:.3f}".format((time.time() - start_time) * 100)) + 'ms',
                             (35, 65), self.font, 1, (0, 0, 255), 2,
                             cv2.LINE_4)
-                cv2.imshow('Live Test', image)
+                cv2.imshow('Live Test', frame)
+                cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
 
                 if cv2.waitKey(5) & 0xFF == ord('q'):
                     self.on_destroy()
