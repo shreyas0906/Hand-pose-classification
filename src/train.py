6f401958bbf84219787b323a2bbc72748ad3049a
shreyas0906 <shreyas0906@gmail.com>
2021-11-01 14:56:52 +0530

    added test_csv and labels, printing accuracy with confusion matrix.

--- src/train.py
+++ src/train.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 from tensorflow.keras.models import load_model, save_model
 
 import models
